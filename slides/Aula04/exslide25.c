ad8105e2dfbcea6f47d6a44e6e9549079351dd83
Victor A. P. Oliveira <victorandreoliveira@gmail.com>
2024-10-24 10:48:28 -0300

    Swap ex25 e ex26

--- slides/Aula04/exslide25.c
+++ slides/Aula04/exslide25.c
@@ -3,8 +3,16 @@
 
 int main() {
     char s1[40] = "O rato roeu a", s2[30] = " roupa do rei de Roma.";
+    int i = 0, j = 0;    
 
-    strcat(s1,s2);
+    while (s1[i]) i++;
+
+    while (s2[j]) { 
+        s1[i] = s2[j];
+        i++;
+        j++;
+    }
+    s1[i] = '\0';
 
     printf("%s",s1);
 
--- slides/Aula04/exslide26.c
+++ slides/Aula04/exslide26.c
@@ -3,16 +3,8 @@
 
 int main() {
     char s1[40] = "O rato roeu a", s2[30] = " roupa do rei de Roma.";
-    int i = 0, j = 0;    
 
-    while (s1[i]) i++;
-
-    while (s2[j]) { 
-        s1[i] = s2[j];
-        i++;
-        j++;
-    }
-    s1[i] = '\0';
+    strcat(s1,s2);
 
     printf("%s",s1);
 
