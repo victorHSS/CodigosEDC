23ca7b6e88d0eb47df66d207081caffe5a0436dc
Victor A. P. Oliveira <victorandreoliveira@gmail.com>
2024-12-20 11:23:39 -0300

    Refatorando diretórios

--- apostila/FundamentosC-Parte1/ex2-saidacomprintf.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    printf("O imundo!");
-
-    return 0;
-}
--- apostila/FundamentosC-Parte1/ex4-saidacomexpressao.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    int num = 100;
-
-    printf("Continha: %d", num + 100);
-
-    return 0;
-}
